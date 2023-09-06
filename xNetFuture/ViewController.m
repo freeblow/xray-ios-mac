6e9b8579c6d4bc38c716d58d53a8623d919f940d
badwin <yangyudong@linkv.sg>
2023-09-06 11:20:36 +0800

    fix title error

--- xNetFuture/ViewController.m
+++ xNetFuture/ViewController.m
@@ -95,7 +95,7 @@ NSString *const kYDApplicationVPNListKey = @"kYDApplicationVPNListKey";
     [super viewDidLoad];
     // Do view setup here.
     self.vpnTextField.delegate = self;
-    self.vpnTextField.stringValue = @"";
+    self.vpnTextField.stringValue = @"vmess://ew0KICAidiI6ICIyIiwNCiAgInBzIjogIl9VU1/nvo7lm70iLA0KICAiYWRkIjogInNpbmdhcG9yZS5jb20iLA0KICAicG9ydCI6ICI0NDMiLA0KICAiaWQiOiAiNmRlZGRiN2YtZTU1Ny00MmRiLWJmYTAtY2Y0MGIzNmIyN2UyIiwNCiAgImFpZCI6ICIwIiwNCiAgInNjeSI6ICJhdXRvIiwNCiAgIm5ldCI6ICJ3cyIsDQogICJ0eXBlIjogIm5vbmUiLA0KICAiaG9zdCI6ICJkLmZyZWVoMS54eXoiLA0KICAicGF0aCI6ICIvZG9uZ3RhaXdhbmcuY29tIiwNCiAgInRscyI6ICJ0bHMiLA0KICAic25pIjogIiIsDQogICJhbHBuIjogIiINCn0=";
     self.startConnectButton.wantsLayer = YES;
     self.startConnectButton.layer.backgroundColor = [NSColor colorWithRed:2/255.0 green:187/255.0 blue:0/255.0 alpha:1.0].CGColor;
     self.startConnectButton.contentTintColor = [NSColor whiteColor];
@@ -533,13 +533,13 @@ NSString *const kYDApplicationVPNListKey = @"kYDApplicationVPNListKey";
     if (connected) {
         self.statusLab.textColor = [NSColor systemGreenColor];
         self.statusLab.stringValue = @"Connected";
-        self.startConnectButton.title = @"Disconnect";
+        self.startConnectButton.title = NSLocalizedString(@"Disconnect", nil);
         self.startConnectButton.layer.backgroundColor = [NSColor systemRedColor].CGColor;
     }
     else {
         self.statusLab.textColor = [NSColor systemRedColor];
         self.statusLab.stringValue = @"Disconnected";
-        self.startConnectButton.title = @"Connect";
+        self.startConnectButton.title = NSLocalizedString(@"Connect", nil);
         self.startConnectButton.layer.backgroundColor = [NSColor colorWithRed:2/255.0 green:187/255.0 blue:0/255.0 alpha:1.0].CGColor;
     }
 }
@@ -556,7 +556,7 @@ NSString *const kYDApplicationVPNListKey = @"kYDApplicationVPNListKey";
             self.statusLab.textColor = [NSColor systemGreenColor];
             self.statusLab.stringValue = @"Connected";
             
-            self.startConnectButton.title = @"Disconnect";
+            self.startConnectButton.title = NSLocalizedString(@"Disconnect", nil);
             self.startConnectButton.layer.backgroundColor = [NSColor systemRedColor].CGColor;
         }
             break;
@@ -571,7 +571,7 @@ NSString *const kYDApplicationVPNListKey = @"kYDApplicationVPNListKey";
             self.statusLab.textColor = [NSColor systemRedColor];
             self.statusLab.stringValue = @"Disconnected";
             
-            self.startConnectButton.title = @"Connect";
+            self.startConnectButton.title = NSLocalizedString(@"Connect", nil);
             self.startConnectButton.layer.backgroundColor = [NSColor colorWithRed:2/255.0 green:187/255.0 blue:0/255.0 alpha:1.0].CGColor;
         }
             break;
